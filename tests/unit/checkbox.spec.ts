26bdb5be03b9c463e0b455e7d5c97d90518cd979
eh. sh <sh.ehsan74.es@gmail.com>
2022-02-27 18:59:23 +0330

    test event emit

--- tests/unit/checkbox.spec.ts
+++ tests/unit/checkbox.spec.ts
@@ -1,11 +1,23 @@
 import { mount } from '@vue/test-utils';
 import Checkbox from '@/components/Checkbox.vue';
 
-describe('label text ', () => {
+describe('label text', () => {
   it('renders props.label when passed', () => {
     const wrapper = mount(Checkbox, {
       props: { label: 'vue' },
     });
     expect(wrapper.find('label').text()).toMatch('vue');
+  });
+});
+
+describe('update:modelValue emitter', () => {
+  it('change state and emit state to on click', () => {
+    const wrapper = mount(Checkbox, {
+      props: { modelValue: false },
+    });
+
+    wrapper.vm.onLabelClick();
+
+    expect(wrapper.emitted()['update:modelValue'][0]).toEqual([true]);
   });
 });
