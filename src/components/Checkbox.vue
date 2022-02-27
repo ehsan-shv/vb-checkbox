b3cddb493747074236abf48a3898f1851a2ca482
eh. sh <sh.ehsan74.es@gmail.com>
2022-02-27 12:30:30 +0330

    starting cleanup

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="vb-checkbox"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Checkbox',
+  props: {},
+});
+</script>
+
+<style lang="scss"></style>
