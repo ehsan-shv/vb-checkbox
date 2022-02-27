4e5f81ec671f39b44b0c8e7536025bac1c2a36b3
eh. sh <sh.ehsan74.es@gmail.com>
2022-02-27 19:28:20 +0330

    publish on npm

---
+++ vb-checkbox/dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VbCheckbox from '@/vb-checkbox.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VbCheckbox,
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <vb-checkbox label="checkbox" />
+  </div>
+</template>
